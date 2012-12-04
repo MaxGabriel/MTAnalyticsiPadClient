476bf62141066754e90ae02a18048eff09beb9cc
Maximilian Tagher <feedback.tagher@gmail.com>
2012-12-03 21:22:02 -0800

    Initial Commit

---
+++ MTAnalyticsiPad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MTAnalyticsiPad
+//
+//  Created by Maximilian Tagher on 12/3/12.
+//  Copyright (c) 2012 Max. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
